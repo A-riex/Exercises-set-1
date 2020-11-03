205b994bc2d95718a956617de90a492b0a4ee144
Riex <ralexander@live.se>
2020-11-03 10:46:44 +0100

    Exercise 1 complete. Bad formatting

--- Exercises_1/Exercise1.cpp
+++ Exercises_1/Exercise1.cpp
@@ -4,33 +4,91 @@
 
 using namespace std;
 
+int main() {
+	// Uppgift a)
 
-int main(){
+	// Translate from first and last character to lowest and max int
+	int c_low = numeric_limits<char>::lowest();
+	int c_max = numeric_limits<char>::max();
 
-
-	cout<< "Min \t \t \t Max \t \t \t Bytes" << endl
-		<< numeric_limits<char>::lowest() << "\t"
-		<< numeric_limits<char>::max() << "\t"
+	// Output char sizes
+	cout << "type \t \t Min \t \t \t Max \t \t \t Bytes" << endl << endl
+		<< "char \t \t"
+		<< c_low << setw(25)
+		<< c_max << setw(25)
 		<< sizeof(char) << endl
 
-		<< numeric_limits<short>::lowest() << "\t"
-		<< numeric_limits<short>::max() << "\t"
+		// Output short sizes
+		<< "short \t \t"
+		<< numeric_limits<short>::lowest() << setw(25)
+		<< numeric_limits<short>::max() << setw(25)
 		<< sizeof(short) << endl
 
-		<< numeric_limits<int>::lowest() << "\t"
-		<< numeric_limits<int>::max() << "\t"
+		// Output int sizes
+		<< "int \t \t"
+		<< numeric_limits<int>::lowest() << setw(25)
+		<< numeric_limits<int>::max() << setw(25)
 		<< sizeof(int) << endl
 
-		<< numeric_limits<long>::lowest() << "\t"
-		<< numeric_limits<long>::max() << "\t"
+		// Output long sizes
+		<< "long \t \t"
+		<< numeric_limits<long>::lowest() << setw(25)
+		<< numeric_limits<long>::max() << setw(25)
 		<< sizeof(long) << endl
 
-		<< numeric_limits<long long>::lowest() << "\t"
-		<< numeric_limits<long long>::max() << "\t"
+		// Output long long sizes
+		<< "long long \t \t"
+		<< numeric_limits<long long>::lowest() << setw(25)
+		<< numeric_limits<long long>::max() << setw(25)
 		<< sizeof(long long) << endl
 
-		<< numeric_limits<unsigned int>::lowest() << "\t"
-		<< numeric_limits<unsigned int>::max() << "\t"
-		<< sizeof(unsigned int) << endl;
+		// Output unsigned int sizes
+		<< "unsigned int \t \t"
+		<< numeric_limits<unsigned int>::lowest() << setw(25)
+		<< numeric_limits<unsigned int>::max() << setw(25)
+		<< sizeof(unsigned int) << endl << endl << endl;
+
+	// Uppgift b)
+
+	// Output float sizes
+	cout << "type \t \t Min \t \t \t Max \t \t \t Bytes" << endl << endl
+		<< "float \t \t"
+		<< numeric_limits<float>::lowest() << setw(25)
+		<< numeric_limits<float>::max() << setw(25)
+		<< sizeof(float) << endl
+
+		// Output double sizes
+		<< "double \t \t"
+		<< numeric_limits<double>::lowest() << setw(25)
+		<< numeric_limits<double>::max() << setw(25)
+		<< sizeof(double) << endl
+
+		// Output long double sizes
+		<< "long double \t \t"
+		<< numeric_limits<long double>::lowest() << setw(25)
+		<< numeric_limits<long double>::max() << setw(25)
+		<< sizeof(long double) << endl << endl;
+
+	// Uppgift c)
+	// Kan ej ha negativt värde, "hoppar varvet runt" till unsigned int max -8.
+	unsigned int u_int = -8;
+	cout << "unsigned int = " << u_int << endl
+		<< "cannot be assigned negative values, assignes max value -8"
+		<< endl << endl;
+
+	// Uppgift d)
+	unsigned int u = 0;
+	int i = -8;
+	if (i < u)
+		cout << "smaller" << endl;
+	else
+		cout << "larger" << endl;
+
+	cout << "Warning: comparison of integers of different signs : 'int' and 'unsigned int'"
+		<< endl;
+
+	// Results in Larger. Compiler gives warning:
+	// comparison of integers of different signs : 'int' and 'unsigned int'
+
 
 }
