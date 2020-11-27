dfdc280698a16972ebf7b0bbd0448c2a5bebce23
Alexander Riex <aleri934@student.liu.se>
2020-11-27 12:44:36 +0100

    Continued work

--- Set 3 Exercise 1/Exercise 2.cpp
+++ Set 3 Exercise 1/Exercise 2.cpp
@@ -24,13 +24,28 @@ using array_of_10_to_int = ptr_to_int[10];
 
 
  // A vector with pointers to int
-using vector_to_ptr_int = std::vector<int*>;
+using vector_of_ptr_int = std::vector<int*>;
 
  // A vector of vectors with pointers to int
-using vector_of_vector_ptr_int = std::vector<vector_to_ptr_int>;
+using vector_of_vector_ptr_int = std::vector<vector_of_ptr_int>;
 
 int main() {
 	ptr_to_int ptr{ new int(3) };
 	std::cout << *ptr << std::endl;
+
+	char c{ 'X' };
+	char* ptr_c{ &c };
+	ptr_to_ptr_to_char ptr2{ &ptr_c };
+	std::cout << **ptr2 << std::endl;
+
+	char AC[2] = { 'X', 'Y'};
+	ptr_to_array_of_char ptr_AC{ AC };
+	std::cout << *ptr_AC << " " << ptr_AC[1] << std::endl;
+
+	array_of_10_to_int ptr4{ ptr };
+	std::cout << *ptr4[0] << std::endl;
+
+
+	
 
 }
